e283bfabaf85cb836464afb8d87713a9d47774c7
dragonaeve <dragonaeve@gmail.com>
2018-04-27 20:26:57 -0700

    made api more modular

--- femaapi.py
+++ femaapi.py
@@ -4,41 +4,40 @@ from datetime import datetime
 
 BASE_URI = 'https://www.fema.gov/api/open/v1/DisasterDeclarationsSummaries?$filter={resource}'
 
-#filter=incidentType eq 'Flood' or incidentType eq 'Fire'
-#disasters must be a list of "'disasterName'"
-def filterByType(disasters):
-	filters = []
-	for disaster in disasters:
-		filters.append('incidentType eq ' + disaster)
-	params = ' or '.join(filters)
-	return BASE_URI.format(resource=params)
-'''
-example calls
-ge >= disasters newer than 1969
-declarationDate gt '1969-04-18T04:00:00.000z'
-le <= disasters older than 1969
-declarationDate le '1969-04-18T04:00:00.000z'
-'''
-#declarationDate ge '1969-04-18T00:00:00.000z' and declarationDate le '1969-04-22:00:00.000z'
-def filterByDate(fromdate, todate):
-	fd = fromdate + 'T00:00:00.000z'
-	td = todate + 'T00:00:00.000z'
-	params = 'declarationDate ge \'{}\' and declarationDate le \'{}\''.format(fd,td)
-	return BASE_URI.format(resource=params)
-
-def filterDateType(fromdate, todate, disasters):
-	fd = fromdate + 'T00:00:00.000z'
-	td = todate + 'T00:00:00.000z'
-	filters = []
-	for disaster in disasters:
-		filters.append('incidentType eq ' + disaster)
-	incidents = ' or '.join(filters)
-	dates = 'declarationDate ge \'{}\' and declarationDate le \'{}\''.format(fd,td)
-	params = dates + ' and (' + incidents + ')'
-	return BASE_URI.format(resource=params)
-
 def getjson(uri):
 	result = requests.get(uri)
 	if result.status_code != 200:
 		print('Status: ', result.status_code, ' Error')
 	return result.json()
+
+#create incidentType filter string
+def parseType(disasters):
+	filters = []
+	for disaster in disasters:
+		filters.append('incidentType eq ' + disaster)
+	return ' or '.join(filters)
+
+#filter=incidentType eq 'Flood' or incidentType eq 'Fire'
+#disasters must be a list of "'disasterName'"
+def filterByType(disasters):
+	params = parseType(disasters)
+	return getjson(BASE_URI.format(resource=params))
+
+#create date range filter string
+def parseDates(fromdate, todate):
+	fd = fromdate + 'T00:00:00.000z'
+	td = todate + 'T00:00:00.000z'
+	return 'declarationDate ge \'{}\' and declarationDate le \'{}\''.format(fd,td)
+
+#ge >= disasters newer than
+#le <= disasters older than 1969
+#declarationDate ge '1969-04-18T00:00:00.000z' and declarationDate le '1969-04-22:00:00.000z'
+def filterByDate(fromdate, todate):
+	params = parseDates(fromdate, todate)
+	return getjson(BASE_URI.format(resource=params))
+
+def filterDateType(fromdate, todate, disasters):
+	incidents = parseType(disasters)
+	dates = parseDates(fromdate, todate)
+	params = dates + ' and (' + incidents + ')'
+	return getjson(BASE_URI.format(resource=params))
--- test.py
+++ test.py
@@ -3,13 +3,13 @@ import json
 
 disasters = ["'Human Cause'", "'Chemical'"]
 
-results = fema.getjson(fema.filterByType(disasters))
+results = fema.filterByType(disasters)
 
-from_date = "1969-01-02"
+from_date = "1980-01-02"
 to_date = "2012-01-05"
 
-results2 = fema.getjson(fema.filterByDate(from_date, to_date))
+results2 = fema.filterByDate(from_date, to_date)
 
-results3 = fema.getjson(fema.filterDateType(from_date, to_date, disasters))
+results3 = fema.filterDateType(from_date, to_date, disasters)
 
 print(json.dumps(results3, indent=4, sort_keys=True))
